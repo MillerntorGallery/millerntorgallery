7eddce38d5758aae210733b63a807a7008b175ac
Sebastian Fuchs <sfuchs@projektkater.de>
2016-09-08 11:22:53 +0200

    fix typo3 compatibility

--- typo3conf/ext/callocationgrid/ext_tables.php
+++ typo3conf/ext/callocationgrid/ext_tables.php
@@ -1,7 +1,7 @@
 <?php
 if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 
-t3lib_extMgm::addStaticFile($_EXTKEY,'static/ts/','Location Grid template');
-t3lib_extMgm::addStaticFile($_EXTKEY,'static/css/','Location Grid CSS-styles');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY,'static/ts/','Location Grid template');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY,'static/css/','Location Grid CSS-styles');
 
 ?>
